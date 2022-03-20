c42674212fc9be3ed16bc914f6fd6e6ef2e9a66b
UO271883 <79576586+UO271883@users.noreply.github.com>
2022-03-20 23:50:28 +0100

    Parte 5

--- app/src/main/java/es/imovil/practicapersistencia/BookViewModel.kt
+++ app/src/main/java/es/imovil/practicapersistencia/BookViewModel.kt
@@ -1,10 +1,15 @@
 package es.imovil.practicapersistencia
 
 import android.app.Application
+import android.provider.Telephony.Mms.Part.FILENAME
 import androidx.lifecycle.AndroidViewModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
 
 class BookViewModel (application: Application) : AndroidViewModel(application){
 
+    val app = application
     var bookList = mutableListOf<Book>()
 
     fun getListSize(): Int{
@@ -18,5 +23,30 @@ class BookViewModel (application: Application) : AndroidViewModel(application){
     fun addBook(book: Book): Boolean{
         val libro = Book(book.title, book.author, book.isbn, book.editorial, book.price)
         return bookList.add(libro)
+    }
+
+    fun restoreBookList(){
+        val file = File(app.filesDir, FILENAME)
+        var lista: List<Book>
+        System.out.println("fuera del if restoreBookList")
+        if (file.exists()) {
+            System.out.println("dentro del if")
+            val contenido = file.readText()
+            val listType = object : TypeToken<List<Book>>() {}.type
+            lista = Gson().fromJson(contenido, listType)
+            bookList.addAll(lista)
+        }
+    }
+
+    fun saveBookList(){
+        val file = File(app.filesDir, FILENAME)
+        var cadena = ""
+        System.out.println("fuera del if saveBookList")
+
+
+        System.out.println("====================== Libro 1 ======================" + bookList[0].title)
+        cadena = Gson().toJson(bookList)
+        file.writeText(cadena)
+
     }
 }
--- app/src/main/java/es/imovil/practicapersistencia/MainActivity.kt
+++ app/src/main/java/es/imovil/practicapersistencia/MainActivity.kt
@@ -1,5 +1,6 @@
 package es.imovil.practicapersistencia
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,10 +10,12 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.activity.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.PreferenceManager
 import es.imovil.practicapersistencia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,8 +25,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private val bookViewModel: BookViewModel by viewModels()
 
+
+    override fun onStart() {
+        super.onStart()
+        System.out.println("En onStart")
+        bookViewModel.restoreBookList()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val sp: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val archivename = sp.getString("archivename", "books_repository")
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -37,6 +49,12 @@ class MainActivity : AppCompatActivity() {
         Navigation.setViewNavController(binding.fab, navController)
         binding.fab.setOnClickListener { view ->
             binding.fab.setOnClickListener ( Navigation.createNavigateOnClickListener(R.id.DataEntryFragment))
+        }
+        navHostFragment.navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when(destination.id) {
+                R.id.ListFragment -> binding.fab.visibility = View.VISIBLE
+                else -> binding.fab.visibility = View.GONE
+            }
         }
     }
 
@@ -60,5 +78,10 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        bookViewModel.saveBookList()
     }
 }
